864fa1c15f98b4c4a77304768ae14a1eb6688eab
tiffanyfong <tiffanyfong42@gmail.com>
2016-10-09 22:46:21 -0700

    stared at sample solution

---
+++ src/main/scala/piconot/internal/Empty.scala
@@ -0,0 +1,12 @@
+package piconot.internal
+
+import scala.language.postfixOps
+
+/**
+  * Created by tmf on 10/9/16.
+  */
+object EmptyBot extends PicoWhile("resources" / "empty.txt") {
+
+
+  run
+}
