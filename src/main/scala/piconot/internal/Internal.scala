5a511d7f315b60224b6dbf33dceca9aae393260a
Farhan Attamimi <farhan.attamimi@gmail.com>
2016-10-20 01:06:16 -0700

    Progress on internal

--- src/main/scala/piconot/internal/Internal.scala
+++ src/main/scala/piconot/internal/Internal.scala
@@ -2,9 +2,32 @@ package piconot.internal
 
 
 Object Internal extends App {
+	private val listOfRules = MutableList.empty[Rule]
 
-	def If(something: List[Int], nothing: List[Int]): Surroundings = {
-		var res = Surroundings(Anything,Anything,Anything,Anything)
+	def If(somethingNothingPart: String, goPart: String): Surroundings = {
+		// This is all kind of sketchy right now, but shows a direction we could
+		// go. Basically, this If function would take in a string of instructions
+		// (If("Nothing[N] and Something[W]", "Go North"))) we'd need to parse it
+		// to find whether they specified "Nothing" or "Something" and what they
+		// passed in for "Go".  Then we'd use this to call the appropriate functions
+		// (Nothing, Something, Go) to generate params for our ruleBuilder which
+		// would then create a rule to add to our rulesList, which would then be
+		// used when running the program.
+
+		var dirList = [0, 0, 0, 0]
+		
+		// Create a Surroundings object for the particular state/set of instructions
+		var nothingList= Nothing("N", dirList)
+		val finalSurroundingsList = Something("N", nothinglist)
+
+		// Create the output values
+		val outputState = Go("North")
+
+		// Create some function to build rules to go into listOfRules
+		val rule = ruleBuilder(finalsurroudninglist, outputState)
+		
+		// Create some function to add a rule to the listOfRules field.
+		addRule(rule)
 	}
 
 	def Something(list: String): List[Int] = {
@@ -47,5 +70,7 @@ Object Internal extends App {
     	 
     	 dirList
 	}
+
+	// ruleBuilder
 }
   
