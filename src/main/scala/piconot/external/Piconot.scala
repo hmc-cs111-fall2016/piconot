28d0facbb84652312c348474156ea9039c366aec
hmc-cs-rparamastri <rparamastri@cs.hmc.edu>
2016-10-23 16:07:31 -0700

    error message for syntax errors

--- src/main/scala/piconot/external/Piconot.scala
+++ src/main/scala/piconot/external/Piconot.scala
@@ -4,10 +4,16 @@ import parser.PicoParser
 import picolib.maze.Maze
 import picolib.semantics._
 
+import scala.util.parsing.combinator._
 import scalafx.application.JFXApp
 import scala.io.Source.fromFile
 
 object Piconot extends JFXApp {
+  def doubleError(msg: String) = {
+    println(s"\n$msg\n")  
+    throw new Exception(s"Was not able to parse the code: $msg")
+  }
+
   val args = parameters.raw  // from the sample solution 
 
   if (args.length != 2)
@@ -17,7 +23,12 @@ object Piconot extends JFXApp {
   val maze = Maze(args(0))
   val picode = fromFile(args(1)).mkString
 
-  val rules = PicoParser(picode).get.sortedRules
+  var rules = List[Rule]()
+  PicoParser(picode) match {
+    case PicoParser.Success(r,_)   => rules = r.sortedRules
+    case PicoParser.Failure(msg,_) => doubleError(msg)
+    case PicoParser.Error(msg,_)   => doubleError(msg)
+  }
 
   object Bot extends Picobot(maze, rules) with TextDisplay with GUIDisplay
 
