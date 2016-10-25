1984b14b0ccfd72efb7edac893054cfc7c579317
Justin Chan <justinchan@WL-215-146.WPA.Claremont.Edu>
2016-10-25 01:48:47 -0700

    Incomplete

--- src/main/scala/piconot/external/parser/Parser.scala
+++ src/main/scala/piconot/external/parser/Parser.scala
@@ -1,48 +1,76 @@
-// package piconot.external.parser
+package piconot.external.parser
 
-// import scala.language.postfixOps
-// import scala.util.parsing.combinator.RegexParsers
+import scala.language.postfixOps
+import scala.util.parsing.combinator.RegexParsers
 
-// import picolib.semantics._
+import picolib.semantics._
 
-// /*
-//  * Our Syntax
-//  *
-//  * rule ::= state "if" pattern "go" nextState
-//  * state ::= "North" | "South" | "East" | "West"
-//  * pattern ::= something
-//  *             | nothing
-//  *             | something "and" nothing
-//  * something ::= "something" dir
-//  * nothing ::= "nothing" dir
-//  * dir ::= [N | E | W | S]*
-//  * nextState ::= state
-//  *
-//  *
-//  */
+/*
+ * Our Syntax
+ *
+ * rule ::= state "if" pattern "go" nextState
+ * state ::= "North" | "South" | "East" | "West"
+ * pattern ::= something
+ *             | nothing
+ *             | something "and" nothing
+ * something ::= "something" dir
+ * nothing ::= "nothing" dir
+ * dir ::= [N | E | W | S]*
+ * nextState ::= state
+ *
+ *
+ */
 
-//  object PiconotParser extends RegexParsers {
+object PiconotParser extends RegexParsers {
 
-//    // for parsing comments
-//    override protected val whiteSpace = """(\s|#.*)+""".r
+	// for parsing comments
+	override protected val whiteSpace = """(\s|#.*)+""".r
 
-//    // parsing interface
-//    def apply(s: String): ParseResult[List[Rule]] = parseAll(program, s)
+	// parsing interface
+	def apply(s: String): ParseResult[List[Rule]] = parseAll(program, s)
 
-//    def program: Parser[List[Rule]] = rule*
+	def program: Parser[List[Rule]] = rule*
 
-//    def rule: Parser[Rule] =
-//      state ~ "if" ~ pattern ~ "go" ~ nextState
-//      { case stateNow ~ "if" ~ surroundings ~ "go"  ~ Dir ⇒
-//         Rule(DirToState(stateNow), surroundings, Dir, DirToState(Dir) }
+	def rule: Parser[Rule] =
+		state ~ "if" ~ pattern ~ "go" ~ nextState
+		{ case stateNow ~ "if" ~ surroundings ~ "go"  ~ Dir ⇒
+		  Rule(DirToState(stateNow), surroundings, Dir, DirToState(Dir) }
 
-//    //def state: Parser[State] =
+	def state: Parser[MoveDirection] = 
+		(	("North" ^^^ North)
+		  | ("South" ^^^ South)
+		  | ("East" ^^^ East)
+		  | ("West" ^^^ West)
 
-//    def DirToState(dir: MoveDirection): State =
-//      ( (North ^^^ 0)
-//      | (South ^^^ 1)
-//      | (East ^^^ 2)
-//      | (West ^^^ 3) )
+	def pattern: Parser[Surroundings] =
+		something
+		| nothing
+		| something ~ "and" ~ nothing 
+		| nothing ~ "and" ~ something
+		{ case sth ⇒ something
+		  case nth ⇒ nothing
+		  case sth ~ "and" ~ nth ⇒ 
+		  case nth ~ "and" ~ sth ⇒ } 
 
-//  }
+		// ndirection ~ edirection ~ wdirection ~ sdirection ^^
+		// { case n ~ e ~ w ~ s ⇒ Surroundings(n, e, w, s) }
+
+
+	def something: Parser[Surroundings] = """"?<=Something\\Q(\\E)(.*)(?=\\Q)\\E)""".r ^^ 
+		{ case "Something(" + dir + ")"} ⇒  interpretSomething(dir) }
+
+	def nothing: Parser[Surroundings] = """"?<=Nothing\\Q(\\E)(.*)(?=\\Q)\\E)""".r ^^ 
+		{ case "Nothing(" + dir + ")"} ⇒  interpretNothing(dir) }
+
+	def nextState: Parser[State] = 
+		state
+		{ case dir ⇒ interpretState(dir)}
+
+	def DirToState(dir: MoveDirection): State =
+		( (North ^^^ 0)
+		| (South ^^^ 1)
+		| (East ^^^ 2)
+		| (West ^^^ 3) )
+
+ }
 
---
+++ src/main/scala/piconot/external/parser/Piconot.scala
@@ -0,0 +1,25 @@
+package piconot.external
+import scalafx.application.JFXApp
+import java.io.File
+import picolib.maze.Maze
+import picolib.semantics._
+import piconot.external.parser._
+
+object Piconot {
+    def main(args: Array[String]) {
+    
+    val empty = Maze(args(0))
+    val source = scala.io.Source.fromFile(args(1))
+    val text = try source.mkString finally source.close()
+    val rules = semantics.eval(PiconotParser(text).get)
+
+    object RunBot extends Picobot(empty, rules) with TextDisplay
+  
+//    stage = EmptyBot.mainStage
+  
+    RunBot.run()
+  }
+
+
+}
+
